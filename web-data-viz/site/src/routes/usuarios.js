2812ce8ec6cc3fe3a76b6c19e3d7bec1690d84ba
MariaPaulaBS <maria.bsilva@sptech.school>
2023-06-17 16:06:32 -0300

    Site com Api

---
+++ web-data-viz/site/src/routes/usuarios.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var router = express.Router();
+
+var usuarioController = require("../controllers/usuarioController");
+
+//Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
+router.post("/limpar", function (req, res) {
+    usuarioController.limpar(req, res);
+})
+
+router.post("/cadastrarQuiz", function (req, res) {
+    usuarioController.cadastrarQuiz(req, res);
+})
+
+module.exports = router;
